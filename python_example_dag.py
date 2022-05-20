c0f8a48b1498becf72d413302b713f0e33a07fa0
Svirin Ruslan <91157814+ZacharyAnalyst@users.noreply.github.com>
2022-05-20 19:44:37 +0300

    Create python_example_dag.py

---
+++ python_example_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime   
+
+default_args = {
+  'owner' = 'Svirin Ruslan',
+  'depends of past' = False, 
+  'start date' = datetime(2022, 5, 20),
+  'retries' = 0,
+}
+
+dag = DAG('calc_example',
+      owner = default_args, 
+      catchup = False,
+      schedule_interval='00 20 * * *')
+
+def some_function():
+  return print('some action') 
+
+def bash_function():
+  return echo 'That's correct'
+
+t1 = PythonOperator(
+  task_id = 'realisation of the Py function',
+  python_callable = some_function,
+  dag=dag
+)
+
+t2 = BashOperator(
+  task_id = 'realisation of the Bash function',
+  bash_command = bash_function,
+  dag = dag
+)
+
+t1 >> t2 
+
