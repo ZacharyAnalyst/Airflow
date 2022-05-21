13ea99170132792122dda16b46ab8a680b4b91bb
ZacharyAnalyst <svirin_kr@mail.ru>
2022-05-21 20:26:53 +0300

    Added example dag file

--- python_example_dag.py
+++ python_example_dag.py
@@ -1,37 +1,33 @@
 from airflow import DAG
-from airflow.operators import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from datetime import datetime   
+from datetime import datetime
 
 default_args = {
-  'owner' = 'Svirin Ruslan',
-  'depends of past' = False, 
-  'start date' = datetime(2022, 5, 20),
-  'retries' = 0,
+	'owner': 'zachary',
+	'depends_on_past': False,
+	'start_date': datetime(2022, 5, 22),
+	'retries': 0
 }
 
-dag_2 = DAG('calc_example',
-      owner = default_args, 
-      catchup = False,
-      schedule_interval='00 20 * * *')
+dag = DAG('calc_example',
+      default_args=default_args,
+      catchup=False,
+      schedule_interval='00 20  * * *')
 
 def some_function():
-  return print('some action') 
+    return print('some action')
 
-def bash_function():
-  return echo 'That's correct'
-
-tt1 = PythonOperator(
-  task_id = 'realisation of the Py function',
-  python_callable = some_function,
-  dag=dag_2
+t1 = PythonOperator(
+  task_id='py_func',
+  python_callable=some_function,
+  dag=dag
 )
 
-tt2 = BashOperator(
-  task_id = 'realisation of the Bash function',
-  bash_command = bash_function,
-  dag = dag_2
+t2 = BashOperator(
+  task_id='bash_func',
+  bash_command='echo "Hello, I am a value!"',
+  dag=dag
 )
 
-tt1 >> tt2 
-
+t1 >> t2
